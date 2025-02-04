4898edb50578c3dd0d70bef7642ebeb7e97a11ef
Vitalii Tomenko <vitalikxxx985@gmail.com>
2025-02-04 21:54:04 +0100

    add new funkcion

--- script/script.js
+++ script/script.js
@@ -16,3 +16,18 @@ let cardList = [
 
 let cardSet;
 let board = [];
+
+const shuffleCards = () => {
+    cardSet = cardList.concat(cardList);
+    for (let i = 0; i < cardSet.length; i++) {
+        let j = Math.floor(Math.random() * cardSet.length);
+        let temp = cardSet[i];
+        cardSet[i] = cardSet[j];
+        cardSet[j] = temp;
+    }
+};
+
+const startGame = () => {
+    
+};
+
