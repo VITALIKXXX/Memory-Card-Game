40483adac338bff371687b8e6cbe5ab431646712
Vitalii Tomenko <vitalikxxx985@gmail.com>
2025-02-04 21:36:33 +0100

    add JS cards

---
+++ script/script.js
@@ -0,0 +1,18 @@
+
+
+
+let errors = 0;
+let cardList = [
+    "bruyne",
+    "degaard",
+    "haaland",
+    "hajrizi",
+    "mbappe",
+    "robert",
+    "stars",
+    "vini",
+    "wirtz"
+]
+
+let cardSet;
+let board = [];
