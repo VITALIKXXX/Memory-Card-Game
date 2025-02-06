8e5713f590903956a45d127b6b31aa52443d98aa
Vitalii Tomenko <vitalikxxx985@gmail.com>
2025-02-06 14:35:55 +0100

    Add card images array

---
+++ script/script.js
@@ -0,0 +1,14 @@
+const board = document.querySelector(".board");
+const errorDisplay = document.querySelector(".errors");
+
+const cardImages = [
+    "bruyne",
+    "degaard",
+    "haaland",
+    "hajrizi",
+    "mbappe",
+    "robert",
+    "stars",
+    "vini",
+    "wirtz"
+]
